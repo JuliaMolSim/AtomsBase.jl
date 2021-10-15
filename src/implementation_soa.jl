a3ca7896575c458928258731083fb2afbceff848
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-15 10:25:55 -0400

    Rename implementation_simple to implementation_aos and create implementation_soa as analogous copy of it with a few things redefined. This precompiles, but I still want to check some things...
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

---
+++ src/implementation_soa.jl
@@ -0,0 +1,34 @@
+# Example implementation using as few function definitions as possible
+#
+using StaticArrays
+
+export SoAAtom, SoASystem, SoAAtomicSystem
+
+struct SoAAtom{D} <: AbstractAtom
+    position::SVector{D, Unitful.Length}
+    element::ChemicalElement
+end
+SoAAtom(position, element)  = SoAAtom{length(position)}(position, element)
+get_position(atom::SoAAtom) = atom.position
+get_element(atom::SoAAtom)  = atom.element
+
+function SoAAtom(position, symbol::Union{Integer,AbstractString,Symbol,AbstractVector})
+    SoAAtom(position, ChemicalElement(symbol))
+end
+
+# static number of particles (not necessary, but simple)
+struct SoASystem{N<:Integer, D, ET<:AbstractElement, AT<:AbstractParticle{ET}} <: AbstractSystem{D,ET,AT}
+    box::SVector{D, SVector{D, Unitful.Length}}
+    boundary_conditions::SVector{D, BoundaryCondition}
+    positions::SMatrix{N,D,Unitful.Length}
+    elements::SVector{N,ET}
+end
+get_box(sys::SoASystem) = sys.box
+get_boundary_conditions(sys::SoASystem) = sys.boundary_conditions
+
+# Base.size(sys::SoASystem) = size(sys.particles)
+Base.length(::SoASystem{N,D,ET,AT}) where {N,D,ET,AT} = N
+
+# first piece of trickiness: can't do a totally abstract dispatch here because we need to know the signature of the constructor for AT
+Base.getindex(sys::SoASystem{SoAAtom{D},D,N}, i::Int) where {D,N} = SoAAtom{D}(sys.positions[i,:],sys.elements[i])
+
