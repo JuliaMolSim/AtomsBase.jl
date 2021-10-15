3454f1ac7d9567e1ad6537095dd5ceb364b28429
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-15 10:25:55 -0400

    Rename implementation_simple to implementation_aos and create implementation_soa as analogous copy of it with a few things redefined. This precompiles, but I still want to check some things...
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/AtomsBase.jl
+++ src/AtomsBase.jl
@@ -1,7 +1,8 @@
 module AtomsBase
 
 include("interface.jl")
-include("implementation_simple.jl")
+include("implementation_soa.jl")
+include("implementation_aos.jl")
 
 # Write your package code here.
 
