35c4f0aef4be4fb02586a9756552d0880e4c14c5
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-15 10:25:55 -0400

    Rename implementation_simple to implementation_aos and create implementation_soa as analogous copy of it with a few things redefined. This precompiles, but I still want to check some things...
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

---
+++ src/implementation_aos.jl
@@ -0,0 +1,30 @@
+# Example implementation using as few function definitions as possible
+#
+using StaticArrays
+
+export AoSAtom, AoSSystem, AoSAtomicSystem
+
+struct AoSAtom{D} <: AbstractAtom
+    position::SVector{D, Unitful.Length}
+    element::ChemicalElement
+end
+AoSAtom(position, element)  = AoSAtom{length(position)}(position, element)
+get_position(atom::AoSAtom) = atom.position
+get_element(atom::AoSAtom)  = atom.element
+
+function AoSAtom(position, symbol::Union{Integer,AbstractString,Symbol,AbstractVector})
+    AoSAtom(position, ChemicalElement(symbol))
+end
+
+# TODO Switch order of type arguments?
+struct AoSSystem{D, ET<:AbstractElement, AT<:AbstractParticle{ET}} <: AbstractSystem{D,ET,AT}
+    box::SVector{D, SVector{D, Unitful.Length}}
+    boundary_conditions::SVector{D, BoundaryCondition}
+    particles::Vector{AT}
+end
+get_box(sys::AoSSystem) = sys.box
+get_boundary_conditions(sys::AoSSystem) = sys.boundary_conditions
+
+Base.size(sys::AoSSystem) = size(sys.particles)
+Base.length(sys::AoSSystem) = length(sys.particles)
+Base.getindex(sys::AoSSystem, i::Int) = getindex(sys.particles, i)
