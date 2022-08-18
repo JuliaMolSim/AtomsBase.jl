59393347df9590ac8de75598203e1d2658b307d9
Jan Habscheid <Jan.Habscheid@rwth-aachen.de>
2022-08-18 17:25:27 +0200

    added tests for system.data

--- test/atom.jl
+++ test/atom.jl
@@ -36,15 +36,18 @@ using Test
     @testset "flexible atomic systems" begin
         box = [[10, 0.0, 0.0], [0.0, 5, 0.0], [0.0, 0.0, 7]]u"Å"
         bcs = [Periodic(), DirichletZero(), DirichletZero()]
+        dic = Dict{String, Any}("extradata_dic"=>"44")
         atoms = [:Si => [0.0, 1.0, 1.5]u"Å",
                  :C  => [0.0, 0.8, 1.7]u"Å",
                  Atom(:H, zeros(3) * u"Å", ones(3) * u"bohr/s")]
-        system = atomic_system(atoms, box, bcs)
+        system = atomic_system(atoms, box, bcs, extradata=45; dic)
         @test length(system) == 3
         @test atomic_symbol(system) == [:Si, :C, :H]
         @test boundary_conditions(system) == [Periodic(), DirichletZero(), DirichletZero()]
         @test position(system) == [[0.0, 1.0, 1.5], [0.0, 0.8, 1.7], [0.0, 0.0, 0.0]]u"Å"
         @test velocity(system) == [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 1.0, 1.0]]u"bohr/s"
+        @test system.extradata == 45
+        @test system.dic["extradata_dic"] == "44"
 
         # Test update constructor
         newatoms  = [system[1], system[2]]
@@ -57,28 +60,34 @@ using Test
     end
 
     @testset "isolated_system" begin
+        dic = Dict{String, Any}("extradata_dic"=>"47")
         system = isolated_system([:Si => [0.0, 1.0, 1.5]u"Å",
                                   :C  => [0.0, 0.8, 1.7]u"Å",
-                                  Atom(:H, zeros(3) * u"Å")])
+                                  Atom(:H, zeros(3) * u"Å")], extradata=46; dic)
         @test length(system) == 3
         @test atomic_symbol(system) == [:Si, :C, :H]
         @test boundary_conditions(system) == [DirichletZero(), DirichletZero(), DirichletZero()]
         @test position(system) == [[0.0, 1.0, 1.5], [0.0, 0.8, 1.7], [0.0, 0.0, 0.0]]u"Å"
         @test velocity(system) == [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]u"bohr/s"
         @test bounding_box(system) == infinite_box(3)
+        @test system.extradata == 46
+        @test system.dic["extradata_dic"] == "47"
     end
 
     @testset "periodic_system" begin
         box = [[10, 0.0, 0.0], [0.0, 5, 0.0], [0.0, 0.0, 7]]u"Å"
+        dic = Dict{String, Any}("extradata_dic"=>"49")
         atoms = [:Si => [0.0, -0.125, 0.0],
                  :C  => [0.125, 0.0, 0.0],
                  Atom(:H, zeros(3) * u"Å")]
-        system = periodic_system(atoms, box; fractional=true)
+        system = periodic_system(atoms, box, extradata=48; fractional=true, dic)
 
         @test length(system) == 3
         @test atomic_symbol(system) == [:Si, :C, :H]
         @test boundary_conditions(system) == [Periodic(), Periodic(), Periodic()]
         @test position(system) == [[0.0, -0.625, 0.0], [1.25, 0.0, 0.0], [0.0, 0.0, 0.0]]u"Å"
+        @test system.extradata == 48
+        @test system.dic["extradata_dic"] == "49"
     end
 
 
