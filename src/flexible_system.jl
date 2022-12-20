79233a5bc2dc85ed954752195dad42cd1142b297
Michael F. Herbst <info@michael-herbst.com>
2022-12-20 09:18:55 +0100

    convert function

--- src/flexible_system.jl
+++ src/flexible_system.jl
@@ -47,6 +47,7 @@ function FlexibleSystem(system::AbstractSystem;
     extra = system isa FlexibleSystem ? system.data : (; )
     FlexibleSystem(particles, bounding_box, boundary_conditions; extra..., kwargs...)
 end
+Base.convert(::Type{FlexibleSystem}, system::AbstractSystem) = FlexibleSystem(system)
 
 """
     AbstractSystem(system::AbstractSystem; kwargs...)
