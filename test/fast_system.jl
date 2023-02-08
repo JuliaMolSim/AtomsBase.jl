4d5d57f671f953d680e8192032ae491caa993d04
Rashid Rafeek <rashidrafeek@gmail.com>
2023-02-08 13:20:59 +0530

    Tests for logical indexing, implement logical indexing for FastSystem

--- test/fast_system.jl
+++ test/fast_system.jl
@@ -27,10 +27,12 @@ using PeriodicTable
     @test system[1:2] == [AtomView(system, 1), AtomView(system, 2)]
     @test system[[1,2]] == [AtomView(system, 1), AtomView(system, 2)]
     @test system[:] == [AtomView(system, 1), AtomView(system, 2)]
+    @test system[[false,true]] == [AtomView(system, 2)]
     @test system[1, :atomic_number] == 6
     @test system[1:2, :atomic_symbol] == [:C, :C]
     @test system[[1,2], :atomic_symbol] == [:C, :C]
     @test system[:, :atomic_symbol] == [:C, :C]
+    @test system[[false,true], :atomic_number] == [6]
     @test system[2][:position] == system[2, :position]
     @test system[2][:position] == [0.75, 0.75, 0.75]u"m"
     @test haskey(system[1], :position)
