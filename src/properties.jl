ee6028f17749fd1b06559d82742cdf7ab6df3927
Michael F. Herbst <info@michael-herbst.com>
2023-07-02 13:42:47 +0200

    Better chemical formula

--- src/properties.jl
+++ src/properties.jl
@@ -1,6 +1,20 @@
 export chemical_formula
 
 """
+    element_symbol(system)
+    element_symbol(system, index)
+
+Return the symbol corresponding to the elmeent of an atom. Note that
+this may be different than `atomic_symbol` for cases where `atomic_symbol`
+is chosen to be more specific (i.e. designate a special atom).
+"""
+element_symbol(system::AbstractSystem, i::Integer) = element(atomic_symbol(system, i)).symbol
+element_symbol(system::AbstractSystem) = [element(num).symbol for num in atomic_number(system)]
+# Note that atomic_symbol cannot be used here, since this may map
+# to something more specific than the element
+
+
+"""
 Returns the chemical formula of an AbstractSystem as a string.
 """
 function chemical_formula(symbols::AbstractVector{Symbol})
@@ -16,4 +30,5 @@ function chemical_formula(symbols::AbstractVector{Symbol})
     end
     join(sort(parts))
 end
-chemical_formula(system) = chemical_formula(atomic_symbol(system))
+
+chemical_formula(system::AbstractSystem) = chemical_formula(element_symbol(system))
