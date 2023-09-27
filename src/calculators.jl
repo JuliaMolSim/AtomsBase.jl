2249099da0afd823e4b53df4a9fc5c499d1c2a17
Christoph Ortner <christohortner@gmail.com>
2023-09-26 20:56:17 -0700

    prototype e, f, v

---
+++ src/calculators.jl
@@ -0,0 +1,19 @@
+
+
+function potential_energy end 
+
+function forces end 
+
+function forces! end 
+
+function virial end 
+
+function energy_forces_virial end 
+
+function energy_forces_virial! end 
+
+
+
+
+
+
