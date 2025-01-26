96495b9343f6d6cf061e513d29cadaba7cb6a0cd
Michael F. Herbst <info@michael-herbst.com>
2025-01-26 16:46:10 +0100

    Bugfixes related to chemical_formula (#133)

--- test/properties.jl
+++ test/properties.jl
@@ -11,34 +11,33 @@ using UnitfulAtomic
     @test chemical_formula([:Ga, :N, :O, :H, :H]) == "GaH₂NO"
 end
 
-@testset "Chemical Species" begin 
+@testset "Chemical Species" begin
     s = ChemicalSpecies(:C)
     @test atomic_number(s) == 6
     @test atomic_symbol(s) == :C
     s1 = ChemicalSpecies(:C; n_neutrons=7)
-    s2 = ChemicalSpecies(:C13) 
+    s2 = ChemicalSpecies(:C13)
     @test s1 == s2
     @test atomic_number(s1) == 6
     @test atomic_symbol(s1) == :C13
 
-    s3 = ChemicalSpecies(:D) 
+    s3 = ChemicalSpecies(:D)
     @test atomic_number(s3) == 1
     @test element_symbol(s3) == :H
     @test s3.n_neutrons == 1
-end 
+end
 
 @testset "Chemical formula with system" begin
     lattice     = tuple([12u"bohr" * rand(3) for _ in 1:3]...)
-    atoms       = [Atom(:C13, randn(3)u"Å"), 
-                   Atom(:C14, randn(3)u"Å"), 
-                   Atom(:D, randn(3)u"Å" ),  
-                   Atom(:D, randn(3)u"Å" ),  
-                   Atom(:D, randn(3)u"Å" ),  
+    atoms       = [Atom(:C13, randn(3)u"Å"),
+                   Atom(:C14, randn(3)u"Å"),
+                   Atom(:D, randn(3)u"Å" ),
+                   Atom(:D, randn(3)u"Å" ),
+                   Atom(:D, randn(3)u"Å" ),
+                   Atom(:X, randn(3)u"Å" ),
                   ]
     system = periodic_system(atoms, lattice)
-    @test species(system, :) == ChemicalSpecies.([:C13, :C14, :D, :D, :D])
-    @test element_symbol(system, :) == [:C, :C, :H, :H, :H]
-    @test chemical_formula(system) == "C₂H₃"
+    @test species(system, :) == ChemicalSpecies.([:C13, :C14, :D, :D, :D, :X])
+    @test element_symbol(system, :) == [:C, :C, :H, :H, :H, :X]
+    @test chemical_formula(system) == "C13C14D₃X"
 end
-
-
